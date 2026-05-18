b379a0b1651ed71cc91b1ce3e0f2efd28924baac
mariofreire7708 <mariofreire7708@gmail.com>
2026-05-18 14:29:31 +0100

    Delete rm_gangs/modules/framework/esx directory

--- rm_gangs/modules/framework/esx/esx_gangs.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS `rm_gangs_esx_players` (
-	`identifier` VARCHAR(60) NOT NULL,
-	`name` VARCHAR(50) NULL DEFAULT 'none',
-	`grade` SMALLINT(5) NULL DEFAULT '0',
-	PRIMARY KEY (`identifier`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET='utf8mb4' COLLATE='utf8mb4_unicode_ci';
